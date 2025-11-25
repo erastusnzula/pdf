6b1e8b09746b606a01bc37f7a3ae48e8730ec6a3
erastusnzula <erastusnzula@gmail.com>
2025-11-25 12:21:11 +0300

    register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from accounts import views
 
 app_name = 'accounts'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register',views.Register.as_view(), name='register' ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+
+class Register(View):
+    def get(self, *args, **kwargs):
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(self.request, 'registration/register.html', context)
+
+    def post(self, *args, **kwargs):
+        form = UserCreationForm(data=self.request.POST)
+        context = {'form': form}
+        if form.is_valid():
+            new_user = form.save()
+            login(self.request, new_user)
+            return redirect('pdf:home-view')
+        return render(self.request, 'registration/register.html', context)
