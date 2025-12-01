5bf02d3386c588957bc00e58694b8f487642bf1a
erastusnzula <erastusnzula@gmail.com>
2025-12-01 21:28:56 +0300

    changes

---
+++ create_superuser.py
@@ -0,0 +1,18 @@
+import os
+import django
+django.setup()
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print('Superuser created')
+    else:
+        print('Superuser already exists')
+else:
+    print('Missing env vars for superuser')
