087f6a79da8d14b231f79a8c8faebc32bb2dbcd4
erastusnzula <erastusnzula@gmail.com>
2025-12-01 22:19:53 +0300

    changes

--- create_superuser.py
+++ create_superuser.py
@@ -10,9 +10,9 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-username = settings.username
-email = settings.email
-password = settings.password
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
 if username and email and password:
     if not User.objects.filter(username=username).exists():
